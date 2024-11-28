b9e323622c26c0fec13f5ec39cfec2ef0af7f8f9
Mellgsouza <0000110119697Xsp@al.educacao.sp.gov.br>
2024-11-28 10:58:45 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'autora',
+    'quem é Collen Hoover?',
+    'Collen Hoover é uma autora americana famosa por romanes emocionantes, que exploram amor, traumas e superação'
+)
+
+criaCartao(
+    'Livros',
+    'Principais gêneros dos seus livros?',
+    'Os principais são romance contemporâneo e drama romântico'
+)
+
+criaCartao(
+    'Vendas',
+    'livro mais vendido',
+    'O livro mais vendido é, É assim que acaba, um fenômeno global sobre temas sensíveis'
+)
+criaCartao(
+    'temas',
+    'Qual o tema mais comum abordado nos livros de Collen Hoover?',
+    'O tema mais comum nos livros de Collen Hoover é o amor unido a traumas e superação'
+)
